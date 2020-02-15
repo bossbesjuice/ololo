ae63f6c7a848636091b84c49e0b254c4a70c1de8
bossbesjuice <Sv3nnyBoii@gmail.com>
2020-02-15 15:48:27 +0100

    jeg har lavet et eksempel

---
+++ ololo.rb
@@ -0,0 +1,18 @@
+# jeg elsker bosbes
+# puts behøver ikke paranteser
+puts "jeg elsker ..."
+
+kage = "kajkage"
+puts "min yndlingskage er " + kage
+
+puts "hvad er din yndlingskage?" 
+
+kage2 = gets
+
+puts "din yndlingskage er " + kage2
+
+if kage == kage2.strip
+    puts "vi har den samme yndlingskage :)"
+else
+    puts "vi har ikke den samme yndlingskage :("
+end
